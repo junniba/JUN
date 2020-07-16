f0e4710883d302a0ff165c8f9fa50ba074c40e26
军ok啦 <912781541@qq.com>
2020-07-16 22:08:29 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MyClient.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+import com.example.mylibrary.JUN;
+
+public class MyClient
+{
+	public static void main(String[] args)throws Exception
+	{
+		new JUN().print();
+	}
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/JUN.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+/**
+ *
+ */
+public class JUN {
+    public void print(){
+        System.out.println("输出为:JUN");
+    }
+}
